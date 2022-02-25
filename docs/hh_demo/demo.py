6c0e208f91f32f493440664e8f95fd9f881cbc3b
notsue <99675115+notsue@users.noreply.github.com>
2022-02-25 11:48:57 +0100

    config missing highlight-css

--- docs/hh_demo/demo.py
+++ docs/hh_demo/demo.py
@@ -4,7 +4,7 @@ sys.path.append('../../')
 import hh
 
 config = {'sourcefile':'demo.hh', 'outputfile':None, 'toc':2, 'h1':0, 'numbers': 1, 'includes': 3 , 'x3d': 1, 
-          'css':'default', 'katex':1, 'highlight': 1, 'dir':'ltr', 'lang':'en', 'class': 'hh', 'view': 1}
+          'css':'default', 'katex':1, 'highlight': 1, 'highlight-css':'github.min.css',  'dir':'ltr', 'lang':'en', 'class': 'hh', 'view': 1}
     
 doc = hh.Document(config)
 print(doc.dublin_core['dc.title'])
